c694bee274e4fc0b4b4e5858a339cb5073be3204
HELFER Thomas 202608 <thomas.helfer@cea.fr>
2020-01-13 17:17:30 +0100

    new tests

---
+++ main.cxx
@@ -0,0 +1,13 @@
+/*!
+ * \file   main.cxx
+ * \brief    
+ * \author HELFER Thomas 202608
+ * \date   13 janv. 2020
+ */
+
+#include<cstdlib>
+#include<boost/python.hpp>
+
+int main(){
+  return EXIT_SUCCESS;
+}
